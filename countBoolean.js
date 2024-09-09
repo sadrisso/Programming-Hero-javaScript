e8ada965a2d3c0501f4c0d0e4855da83d98bc796
sadrisso <sadriisso304@gmail.com>
2024-09-09 15:27:46 +0600

    count the boolean values

---
+++ countBoolean.js
@@ -0,0 +1,22 @@
+
+
+const x = [true,false, 22, 43, "Drisso", "hello", {name: "Masum", age: 34}];
+
+
+function countBoolean (arr) {
+    if (!Array.isArray(arr)) {
+        return "This is not an array.."
+    }
+
+    let count = 0;
+
+    for (let res of arr) {
+        if (typeof res === "boolean") {
+            count++;
+        }
+    }
+    return count;
+}
+
+const result = countBoolean(x);
+console.log(result);
