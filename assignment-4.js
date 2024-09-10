355a89e8b8e7b09127c79a3a5c68339d3d6ed0f8
sadrisso <sadriisso304@gmail.com>
2024-09-10 22:54:03 +0600

    problem-2,3 and 4 done

--- assignment-4.js
+++ assignment-4.js
@@ -1,4 +1,5 @@
 
+
 function calculateTax(income, expense) {
     if (expense > income) {
         return "Invalid input!";
@@ -10,3 +11,57 @@ function calculateTax(income, expense) {
     return tax;
 }
 
+
+
+function sendNotification (email) {
+
+    const arr = email.split('@');
+
+    const notification = arr[0] + " sent you an email from " + arr[1];
+    return notification;
+}
+
+
+
+
+function checkDigitsInName(name) {
+    if (typeof name !== "string") {
+        return "Invalid Input";
+    }
+
+    for (let i = 0; i <= name.length; i++) {
+        if (!isNaN(name[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+function calculateFinalScore(obj) {
+    if (typeof obj !== "object") {
+        return "Invalid Input";
+    }
+
+    let totalMarks = obj.testScore + obj.schoolGrade;
+
+    if (obj.isFFamily) {
+        totalMarks += 20;
+    }
+    else {
+        totalMarks = totalMarks;
+    }
+
+    if (totalMarks >= 80) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+const result = calculateFinalScore({name: "Rakib", testScore: 40, schoolGrade: 20, isFFamily: true});
+console.log(result)
