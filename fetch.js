bd83584650d0d8780062953c580b61c5437b196b
sadrisso <sadriisso304@gmail.com>
2024-10-03 00:21:52 +0600

    fetch data and display it on the browser

--- fetch.js
+++ fetch.js
@@ -8,3 +8,50 @@ function loadData ()
     .then(json => console.log(json))
 }
 
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+function loadData2 ()
+{
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data));
+}
+
+function displayData (data)
+{
+    const ul = document.querySelector(".user-container");
+    for (const user of data) {
+        console.log(user.name)
+        console.log(ul)
+        const li = document.createElement("li");
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
+}
+
+
+const postUrl = "https://jsonplaceholder.typicode.com/posts";
+
+function loadData3 () {
+    fetch(postUrl)
+    .then(res => res.json())
+    .then(data => displayData3(data))
+}
+
+function displayData3 (data) {
+    const postsContainer = document.getElementById("posts-container")
+    for (res of data) {
+        console.log(res)
+        const post = document.createElement("div");
+        post.classList.add("post")
+        post.innerHTML = `
+            <h3>${res.userId}</h3>
+            <h2>${res.title}</h2>
+            <p>${res.body}</p>
+        `
+        postsContainer.appendChild(post)
+    }
+}
+
+loadData3()
