d8601f8860629c4b4c081d843d98f9f93599c9fb
sadrisso <sadriisso304@gmail.com>
2024-10-09 20:30:48 +0600

    fetched buttons category and display on the UI

--- pet-project(1)/js/index..js
+++ pet-project(1)/js/index..js
@@ -1,2 +1,26 @@
 
-const buttons = document.getElementById("categories");
+const buttonsContainer = document.getElementById("categories");
+
+const categoryData = async () =>
+{
+    const res = await fetch("https://openapi.programming-hero.com/api/peddy/categories");
+    const data = await res.json();
+
+    displayAllCategories(data.categories);
+}
+
+
+const displayAllCategories = (categories) =>
+{
+    for (category of categories) {
+        const buttons = document.createElement("div");
+        buttons.innerHTML = `
+            <button class="btn btn-outline">${category.category}</button>
+        `
+        buttonsContainer.append(buttons);
+    }
+}
+
+
+
+categoryData();
