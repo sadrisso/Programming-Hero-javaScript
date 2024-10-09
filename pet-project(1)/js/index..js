0ea250adedc5bfcb19fbe5d23934bf6519528aee
sadrisso <sadriisso304@gmail.com>
2024-10-09 20:46:34 +0600

    fetched all data and display in on the UI

--- pet-project(1)/js/index..js
+++ pet-project(1)/js/index..js
@@ -1,5 +1,6 @@
 
 const buttonsContainer = document.getElementById("categories");
+const allPetDataContainer = document.getElementById("all-pet-data");
 
 const categoryData = async () =>
 {
@@ -22,5 +23,39 @@ const displayAllCategories = (categories) =>
 }
 
 
+const allPetData = async () =>
+{
+    const res = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
+    const data = await res.json();
+
+    console.log(data.pets)
+    displayAllPets(data.pets)
+}
+
+const displayAllPets = (data) =>
+{
+    data.forEach((d) => {
+        console.log(d)
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <div class="card card-compact bg-base-100 w-96 shadow-xl">
+            <figure>
+                <img src="${d.image}" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+            </div>
+        `
+        allPetDataContainer.append(card);
+    })
+}
+
+
 
 categoryData();
+allPetData();
