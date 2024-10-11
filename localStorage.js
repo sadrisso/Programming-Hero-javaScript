dcc6abef22e0cc3dc6cfcec2091b418acfa491cd
sadrisso <sadriisso304@gmail.com>
2024-10-11 22:31:10 +0600

    set products to localStorage

---
+++ localStorage.js
@@ -0,0 +1,48 @@
+
+const setItem = () =>
+{
+    localStorage.setItem("age", 22)
+}
+
+const addProduct = () =>
+{
+    const productField = document.getElementById("product-name");
+    const quantityField = document.getElementById("product-quantity");
+    const product = productField.value;
+    const quantity = quantityField.value;
+
+    productField.value = "";
+    quantityField.value = "";
+
+    displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
+}
+
+
+const displayProduct = (product, quantity) =>
+{
+    const productContainer = document.getElementById("productContainer");
+    const li = document.createElement("li");
+    li.innerText = `${product}: ${quantity}`;
+
+    productContainer.append(li);
+}
+
+const getStoredShoppingCart = () =>
+{
+    const storedCart = localStorage.getItem("cart");
+    let cart = {}
+
+    if(storedCart) {
+        cart = JSON.parse(storedCart)
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) =>
+{
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart)
+    localStorage.setItem("cart", cartStringified)
+}
